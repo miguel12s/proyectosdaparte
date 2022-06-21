1446041c2044a8f740c075b374c91bc2b9d6dd9d
miguel12s <87570624+miguel12s@users.noreply.github.com>
2022-06-21 00:02:05 -0500

    Add files via upload

--- js/registroempresa.js
+++ js/registroempresa.js
@@ -3,7 +3,6 @@ let comprobar=()=>{
     let b= document.getElementById("exampleDropdownFormEmail1").value
     let c= document.getElementById("exampleDropdownFormPassword1").value
     let d= document.getElementById("exampleDropdownFormTelefono").value
-    alert("hola")
     if (a==""||b=="" ||c=="" ||d=="" ){
      alert("los campos estan vacio")
     }else{
